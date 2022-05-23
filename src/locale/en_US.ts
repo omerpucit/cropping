9af12ec829df81666d2449b153dff8fcb7113596
umer bilal <omerpucit@gmail.com>
2022-05-24 00:09:26 +0500

    Update en_US.ts

--- src/locale/en_US.ts
+++ src/locale/en_US.ts
@@ -6,4 +6,5 @@ export default {
   'edit picture': 'Edit Picture',
   'drag to crop': 'Drag to Crop',
   'click to upload': 'Click to upload',
+  'small image': 'The image has been scaled to its maximum scale and cannot be scaled further.'
 }
